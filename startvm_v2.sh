ceab9d3689b1c88dea0cd6513e095ad5c0fdb013
Norvelle <wnorvell@m47607wnorvell.premierinc.com>
2017-06-05 11:13:25 -0400

    startvm.sh - order of uploads changed, deprecated startvm_v2.sh - same as startvm.sh but names snapshots with datetime appended

--- startvm_v2.sh
+++ startvm_v2.sh
@@ -2,25 +2,33 @@
 vagrant up --provision-with bashrc
 vagrant up --provision-with prebuild
 vagrant halt
-vagrant snapshot save prebuild
+DATE=`date +%Y_%m_%d-%H-%M`
+NAME="prebuild_${DATE}"
+vagrant snapshot save $NAME
 osascript -e 'display notification "prebuild done" with title "It done" sound name "Ping"'
 
 vagrant up --provision-with wh_starter
 vagrant up --provision-with build
 vagrant halt
-vagrant snapshot save built
+DATE=`date +%Y_%m_%d-%H-%M`
+NAME="built_${DATE}"
+vagrant snapshot save $NAME
 osascript -e 'display notification "built done" with title "It done" sound name "Ping"'
 
 vagrant up --provision-with sed_script
 vagrant up --provision-with sql
 vagrant halt
-vagrant snapshot save sql
+DATE=`date +%Y_%m_%d-%H-%M`
+NAME="sql_${DATE}"
+vagrant snapshot save $NAME
 osascript -e 'display notification "sql done" with title "It done" sound name "Ping"'
 
 vagrant up --provision-with extra_installs
 vagrant up --provision-with extras
 vagrant halt
-vagrant snapshot save full
+DATE=`date +%Y_%m_%d-%H-%M`
+NAME="full_${DATE}"
+vagrant snapshot save $NAME
 osascript -e 'display notification "full done" with title "It done" sound name "Ping"'
 
 osascript -e 'display notification "VM finished successfully?" with title "It done" sound name "Ping"'
