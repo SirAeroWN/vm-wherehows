23ac020dc5c52aca75f6da022e9d5cd5d71aefd0
Norvelle <wnorvell@m47607wnorvell.premierinc.com>
2017-06-05 11:15:34 -0400

    whvm.sh - abstracts away and simplifies vagrant commands

---
+++ whvm.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# saves snapshot with given name and current date
+save() {
+	DATE=`date +%Y_%m_%d-%H-%M`	
+	NAME="${1}_${DATE}"
+	vagrant snapshot save $NAME
+}
+
+# restores to specified snapshot
+restore() {
+	vagrant snapshot restore $1 --no-provision
+}
+
+# prints list of snapshots
+list() {
+	vagrant snapshot list
+}
+
+# stops vm, boots, and opens
+restart() {
+	vagrant halt
+	vagrant up
+	vagrant ssh
+}
+
+# reboots vm with specified provision
+reconfig() {
+	vagrant halt
+	vagrant up --provision-with $1
+}
+
+# first setup of WhereHows VM
+init() {
+	vagrant halt
+	vagrant --force delete
+	source ./startvm_v2.sh
+}
+
+case $1 in
+	save)
+		save $2
+		;;
+	restore)
+		restore $2
+		;;
+	list)
+		list
+		;;
+	restart)
+		restart
+		;;
+	reconfig)
+		reconfig $2
+		;;
+	init)
+		init
+		;;
+	*)
+		echo "Usage: whvm.sh {save|restore|list|restart|reconfig|init} {snapshot-name|provison-name}"
+		exit 1
+		;;
+esac
