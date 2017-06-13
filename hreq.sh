0ff82a1e1e1239600dac9d5b026c3b3e938e25ef
Will Norvelle <will_norvelle@premierinc.com>
2017-06-13 09:14:44 -0400

    added way to specify port to make request to, pass as last parameter to use default ports, don't pass a port

--- hreq.sh
+++ hreq.sh
@@ -1,35 +1,47 @@
 #!/bin/bash
 
 post() {
-	curl -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d @${1} http://localhost:19001${2}
+	if [ -z "$3" ]; then
+		curl -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d @${1} http://localhost:19001${2}
+	else
+		curl -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d @${1} http://localhost:${3}${2}
+	fi
 }
 
 put() {
-	curl -H "Accept: application/json" -H "Content-Type: application/json" -X PUT -d @${1} http://localhost:19001${2}
+	if [ -z "$3" ]; then
+		curl -H "Accept: application/json" -H "Content-Type: application/json" -X PUT -d @${1} http://localhost:19001${2}
+	else
+		curl -H "Accept: application/json" -H "Content-Type: application/json" -X PUT -d @${1} http://localhost:${3}${2}
+	fi
 }
 
 get() {
-	curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://localhost:19001${2}
+	if [ -z "$2" ]; then
+		curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://localhost:19001${2}
+	else
+		curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://localhost:${3}${2}
+	fi
 }
 
 case $1 in
 	post)
-		post $2 $3
+		post $2 $3 $4
 		;;
 	POST)
-		post $2 $3
+		post $2 $3 $4
 		;;
 	put)
-		put $2 $3
+		put $2 $3 $4
 		;;
 	PUT)
-		put $2 $3
+		put $2 $3 $4
 		;;
 	get)
-		get $2
+		get $2 $3
 		;;
 	GET)
-		get $2
+		get $2 $3
 		;;
 	*)
 		echo "Usage: hreq {post|put|get} {file|URL} {URL}"
